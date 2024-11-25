c321e18f5e56ec3a6c10d42fc42f99259fd489fc
qwaszxjack <jackiechen246@gmail.com>
2024-11-24 21:13:37 -0800

    formatting

--- frontend/src/stories/components/Updates/UpdatesTable.stories.js
+++ frontend/src/stories/components/Updates/UpdatesTable.stories.js
@@ -3,18 +3,18 @@ import UpdatesTable from "main/components/Updates/UpdatesTable.js";
 import { updatesFixtures } from "fixtures/updatesFixtures.js";
 
 export default {
-    title: "components/Updates/UpdatesTable.js",
-    component: UpdatesTable,
+  title: "components/Updates/UpdatesTable.js",
+  component: UpdatesTable,
 };
 
 const Template = (args) => {
-    return <UpdatesTable {...args} />;
+  return <UpdatesTable {...args} />;
 };
 
 export const Empty = Template.bind({});
 
 Empty.args = {
-    updates: [],
+  updates: [],
 };
 
 export const OneUpdate = Template.bind({});
@@ -26,5 +26,5 @@ OneUpdate.args = {
 export const ThreeUpdates = Template.bind({});
 
 ThreeUpdates.args = {
-    updates: updatesFixtures.threeUpdates,
+  updates: updatesFixtures.threeUpdates,
 };
