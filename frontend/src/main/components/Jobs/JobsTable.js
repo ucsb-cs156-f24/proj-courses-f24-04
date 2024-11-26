d13faa3d7d500d45d946d8fca78dd2dd688f4120
Alvin Wang <alvinwang@ucsb.edu>
2024-11-26 00:32:40 -0800

    new

--- frontend/src/main/components/Jobs/JobsTable.js
+++ frontend/src/main/components/Jobs/JobsTable.js
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 import OurTable, {
   PlaintextColumn,
@@ -113,7 +114,6 @@ export default function JobsTable({ jobs }) {
         </tbody>
       </OurTable>
 
-      {/* Pagination Controls */}
       <div>
         <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
           {"<<"}
@@ -142,5 +142,49 @@ export default function JobsTable({ jobs }) {
         </select>
       </div>
     </div>
+  );
+}
+*/
+import React from "react";
+import OurTable, {
+  PlaintextColumn,
+  DateColumn,
+} from "main/components/OurTable";
+
+export default function JobsTable({ jobs }) {
+  const testid = "JobsTable";
+
+  const columns = [
+    {
+      Header: "id",
+      accessor: "id", // accessor is the "key" in the data
+    },
+    DateColumn("Created", (cell) => cell.row.original.createdAt),
+    DateColumn("Updated", (cell) => cell.row.original.updatedAt),
+    {
+      Header: "Status",
+      accessor: "status",
+    },
+    PlaintextColumn("Log", (cell) => cell.row.original.log),
+  ];
+
+  const sortees = React.useMemo(
+    () => [
+      {
+        id: "id",
+        desc: true,
+      },
+    ],
+    // Stryker disable next-line all
+    [],
+  );
+
+  return (
+    <OurTable
+      data={jobs}
+      columns={columns}
+      testid={testid}
+      initialState={{ sortBy: sortees }}
+    />
   );
 }
